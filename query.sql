6ed79d212747db7a5f073baa9ccd323231240236
Taebu <mtaebu@gmail.com>
2022-07-19 01:49:46 +0900

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `codal`.`tableforchat` (
+  `num` INT NOT NULL AUTO_INCREMENT,
+  `chatuser` VARCHAR(30) NULL,
+  `chattext` VARCHAR(1000) NULL,
+  `chatpassword` VARCHAR(10) NULL,
+  `chattime` VARCHAR(50) NULL,
+  PRIMARY KEY (`num`));
